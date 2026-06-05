8a19f7e85c81f7eab2943f5aaa46fa3f2e9c6dd4
Sattwik Sahu <sattwik6002@gmail.com>
2026-06-06 00:49:42 +0530

    Add files via upload

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,45 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String portfolioName;
+
+    @OneToOne
+    private Client client;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(String portfolioName, Client client) {
+        this.portfolioName = portfolioName;
+        this.client = client;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public String getPortfolioName() {
+        return portfolioName;
+    }
+
+    public void setPortfolioName(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
